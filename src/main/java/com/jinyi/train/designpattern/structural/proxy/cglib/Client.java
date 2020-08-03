a36bbad6d13068d0152e850c78d59a38ab6f544c
liujin <1366445904@qq.com>
2020-08-03 15:32:30 +0800

    更新动态代理创建方式

--- src/main/java/com/jinyi/train/designpattern/structural/proxy/cglib/Client.java
+++ src/main/java/com/jinyi/train/designpattern/structural/proxy/cglib/Client.java
@@ -3,6 +3,7 @@ package com.jinyi.train.designpattern.structural.proxy.cglib;
 
 import com.jinyi.train.designpattern.structural.proxy.RealSubject;
 import com.jinyi.train.designpattern.structural.proxy.Subject;
+import org.springframework.cglib.core.DebuggingClassWriter;
 import org.springframework.cglib.proxy.Enhancer;
 
 /**
@@ -13,10 +14,23 @@ import org.springframework.cglib.proxy.Enhancer;
 public class Client {
 
   public static void main(String[] args) {
+    partOne();
+    partTwo();
+  }
+
+  private static void partTwo() {
+    System.getProperties().put(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY, "D:\\workspace\\integration-testing\\aop-guide\\test");
+    System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY, "D:\\workspace\\integration-testing\\aop-guide\\test");
+
+    RealSubject subject1 = (RealSubject) Enhancer.create(RealSubject.class,new DemoMethodInterceptor());
+    subject1.hello();
+  }
+
+  private static void partOne() {
     Enhancer enhancer = new Enhancer();
     enhancer.setSuperclass(RealSubject.class);
     enhancer.setCallback(new DemoMethodInterceptor());
-    Subject subject = (Subject) enhancer.create();
+    RealSubject subject = (RealSubject) enhancer.create();
     subject.hello();
   }
 }
