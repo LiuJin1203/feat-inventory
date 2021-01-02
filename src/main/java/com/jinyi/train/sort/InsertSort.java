ea6fcc8a1c44b553862300b08e6f4b9b4d31b11c
LiuJin <1366445904@qq.com>
2021-01-02 23:38:55 +0800

    feat: 修改插入排序

--- src/main/java/com/jinyi/train/sort/InsertSort.java
+++ src/main/java/com/jinyi/train/sort/InsertSort.java
@@ -12,54 +12,68 @@ import java.util.Arrays;
  * 时间复杂度：O(N²)
  * 空间复杂度：O(1)
  * ***************************************
+ *
  * @author liujin
  * @date 2020/12/27
  * @since JDK8
  */
 public class InsertSort extends AbstractSortBase {
-    int method = 1;
+    int method = 2;
+
     public InsertSort() {
     }
 
     public InsertSort(int method) {
         this.method = method;
     }
-    public static Sort getInstance(int method){
+
+    public static Sort getInstance() {
+        return new InsertSort();
+    }
+    public static Sort getInstance(int method) {
         return new InsertSort(method);
     }
+
     @Override
     public void sort(Comparable[] arr) {
-        if(arr==null||arr.length<2){
+        if (arr == null || arr.length < 2) {
             return;
         }
-        switch (method){
-            case 2:method2(arr);break;
-            default:method1(arr);
+        switch (method) {
+            case 1:
+                method1(arr);
+                break;
+            default:
+                method2(arr);
         }
         show(arr);
     }
 
     /**
      * doBySelf
+     *
      * @param arr
      */
     private void method1(Comparable[] arr) {
         int N = arr.length;
         for (int i = 1; i < N; i++) {
             for (int j = 0; j < i; j++) {
-                if(less(arr[i],arr[j])){
-                    exchage(arr,i,j);
-                }
+                if(less(arr[i],arr[j]))
+                exchage(arr, i, j);
+
             }
         }
     }
+
+    /**
+     * book
+     * @param arr
+     */
     private void method2(Comparable[] arr) {
         int N = arr.length;
         for (int i = 1; i < N; i++) {
-            for (int j = i; j >0; j--) {
-                if(less(arr[j],arr[j-1])){
-                    exchage(arr,j,j-1);
-                }
+            for (int j = i; j > 0 && less(arr[j], arr[j - 1]); j--) {
+                    exchage(arr, j, j - 1);
             }
         }
     }
