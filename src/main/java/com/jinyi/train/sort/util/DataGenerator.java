bcfe5a7ff298e37965499ca3b719ed60e3218a9f
LiuJin <1366445904@qq.com>
2022-04-05 19:56:23 +0800

    feature: 增加二维数组生成函数

--- src/main/java/com/jinyi/train/sort/util/DataGenerator.java
+++ src/main/java/com/jinyi/train/sort/util/DataGenerator.java
@@ -1,8 +1,8 @@
 package com.jinyi.train.sort.util;
 
-import com.jinyi.train.designpattern.creational.singleton.InnerClassSingleton;
-
+import java.util.Arrays;
 import java.util.Random;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 /**
@@ -18,6 +18,20 @@ public class DataGenerator {
         Integer[] arr = new Integer[number];
         for (int i = 0; i < number; i++) {
             arr[i] =  rand.nextInt(number*10)+1;
+        }
+        return arr;
+    };
+    /**
+     * 二维数组的生成器函数
+     */
+    public static BiFunction<Integer,Integer,Comparable[][]> fakeIntegerArr = (row,col)->{
+        Integer[][] arr = new Integer[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                arr[i][j] =  rand.nextInt(row*col*10)+1;
+
+            }
+            Arrays.sort(arr[i]);
         }
         return arr;
     };
